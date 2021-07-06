473b43dbfc205dc28685105b0c6ffb739f84d689
chuan minh <minhchuan75@gmail.com>
2021-07-06 15:48:45 +0700

    Update user entity

--- src/main/java/zuka/cloud/demo/model/User.java
+++ src/main/java/zuka/cloud/demo/model/User.java
@@ -1,38 +1,14 @@
 package zuka.cloud.demo.model;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import zuka.cloud.demo.repository.UserRepository;
 
 import javax.persistence.*;
 import java.io.Serializable;
 
-/*class UserId implements Serializable {
-
-    @Column(name = "id", nullable = false)
-    private int id;
-
-    @Column(name = "role", nullable = false)
-    private String role;
-
-    public UserId(String role, int id) {
-        this.role = role;
-        this.id = id;
-    }
-
-    public UserId() {
-    }
-}*/
-
 @Entity
 @Table(name = "user")
-//@IdClass(UserId.class)
-//@ApiModel(value = "User model")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -43,7 +19,6 @@ public class User implements Serializable {
     @Column(name = "id", nullable = false)
     private int id;
 
-    //@Id
     @Column(name = "role", length = 100, nullable = false)
     private String role;
 
