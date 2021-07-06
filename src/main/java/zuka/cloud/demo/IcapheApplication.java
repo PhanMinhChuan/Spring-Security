39fd088c5d2cfb3630086b1da9de4d3d9dcd97e1
chuan minh <minhchuan75@gmail.com>
2021-07-06 09:30:36 +0700

    Main application

---
+++ src/main/java/zuka/cloud/demo/IcapheApplication.java
@@ -0,0 +1,45 @@
+package zuka.cloud.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import zuka.cloud.demo.model.User;
+import zuka.cloud.demo.repository.UserRepository;
+
+@SpringBootApplication
+public class IcapheApplication implements CommandLineRunner{
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcapheApplication.class, args);
+    }
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user1 = new User();
+        user1.setId(1);
+        user1.setUsername("admin");
+        user1.setPassword(passwordEncoder.encode("123"));
+        user1.setRole("ROLE_ADMIN");
+        userRepository.save(user1);
+
+        User user2 = new User();
+        user2.setId(2);
+        user2.setUsername("user");
+        user2.setPassword(passwordEncoder.encode("123"));
+        user2.setRole("ROLE_USER");
+        userRepository.save(user2);
+    }
+}
+
