eefcc68081d1e83e45c859423eae0f42b6a0f549
chuan minh <minhchuan75@gmail.com>
2021-07-06 15:49:16 +0700

    Update & optimal UserService

--- src/main/java/zuka/cloud/demo/service/UserService.java
+++ src/main/java/zuka/cloud/demo/service/UserService.java
@@ -20,32 +20,22 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        /*User user = userRepository.findByUsername(username);
-        if(!"admin".equalsIgnoreCase(user.getUsername())) throw new UsernameNotFoundException("User name not found");
-        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-        SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_USER");
-        authorities.add(authority);
-        CustomUserDetails customUserDetails = new CustomUserDetails(user.getUsername(), user.getPassword(), authorities);
-        return customUserDetails;*/
         User user = userRepository.findByUsername(username);
         if (user == null) {
             throw new UsernameNotFoundException(username);
         }
         return new CustomUserDetails(user);
-        //List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
-        //grantedAuthorities.add(new SimpleGrantedAuthority(UserRepo.getRole()));
     }
 
     // JWTAuthenticationFilter sẽ sử dụng hàm này
     @Transactional
     public UserDetails loadUserById(int id) {
-        /*User user = userRepository.findById(id);
-        if (user == null) {
-            System.out.println("NULL USER");
-        }*/
-        User user = userRepository.findById(id);/*.orElseThrow(
+        User user = userRepository.findById(id);/*orElseThrow(
                 () -> new UsernameNotFoundException("User not found with id : " + id)
         );*/
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with id : " + id);
+        }
         return new CustomUserDetails(user);
     }
 }
