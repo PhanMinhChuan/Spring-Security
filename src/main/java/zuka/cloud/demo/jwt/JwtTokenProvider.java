33064a6bbc0d0b2699892f99c90f22581a3f6b4f
chuan minh <minhchuan75@gmail.com>
2021-07-09 14:25:18 +0700

    chore: provider token by user username instead of id

--- src/main/java/zuka/cloud/demo/jwt/JwtTokenProvider.java
+++ src/main/java/zuka/cloud/demo/jwt/JwtTokenProvider.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import org.springframework.stereotype.Component;
 import io.jsonwebtoken.*;
 import lombok.extern.slf4j.Slf4j;
-import zuka.cloud.demo.model.CustomUserDetails;
 
 @Component
 @Slf4j
@@ -17,26 +16,26 @@ public class JwtTokenProvider {
     private final long JWT_EXPIRATION = 60000;
 
     // Tạo ra jwt từ thông tin user
-    public String generateToken(CustomUserDetails userDetails) {
+    public String generateToken(org.springframework.security.core.userdetails.User user) {
         // Lấy thông tin user
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + JWT_EXPIRATION);
         // Tạo chuỗi json web token từ id của user.
         return Jwts.builder()
-                .setSubject(Integer.toString(userDetails.getId()))      //"sub"     "sub": "batman"
+                .setSubject(user.getUsername())                         //"sub"     "sub": "batman"
                 .setIssuedAt(now)                                       //"iss"     "iss": "jira:1314039",
                 .setExpiration(expiryDate)                              //"exp"     "exp": 1300819380,
                 .signWith(SignatureAlgorithm.HS512, JWT_SECRET_KEY)
                 .compact();
     }
 
-    public Integer getUserIdFromJWT(String token) {
+    public String getUsernameFromJWT(String token) {
         Claims claims = Jwts.parser()
                 .setSigningKey(JWT_SECRET_KEY)
                 .parseClaimsJws(token)
                 .getBody();
 
-        return Integer.parseInt(claims.getSubject());
+        return claims.getSubject();
     }
 
     public boolean validateToken(String authToken) {
