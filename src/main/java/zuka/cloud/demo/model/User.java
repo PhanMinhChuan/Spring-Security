b449c3a8529a398230ad032b36d944bd52c52344
chuan minh <minhchuan75@gmail.com>
2021-07-09 14:28:20 +0700

    refactor: add more properties and delete role properties

--- src/main/java/zuka/cloud/demo/model/User.java
+++ src/main/java/zuka/cloud/demo/model/User.java
@@ -3,15 +3,21 @@ package zuka.cloud.demo.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 
 @Entity
 @Table(name = "user")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class User implements Serializable {
 
     @Id
@@ -19,13 +25,18 @@ public class User implements Serializable {
     @Column(name = "id", nullable = false)
     private int id;
 
-    @Column(name = "role", length = 100, nullable = false)
-    private String role;
-
     @Column(name = "username", length = 32, nullable = false)
     private String username;
 
     @Column(name = "password", length = 100, nullable = false)
     private String password;
 
+    @CreatedBy
+    private String createdBy;
+
+    @CreatedDate
+    private Date createdAt;
+
+    @LastModifiedDate
+    private Date login;
 }
