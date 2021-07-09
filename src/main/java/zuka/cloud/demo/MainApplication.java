a10561c161613da195138f1ab6ee2336f6751dc7
chuan minh <minhchuan75@gmail.com>
2021-07-09 14:29:18 +0700

    chore: update object before

--- src/main/java/zuka/cloud/demo/MainApplication.java
+++ src/main/java/zuka/cloud/demo/MainApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import zuka.cloud.demo.model.Role;
 import zuka.cloud.demo.model.User;
+import zuka.cloud.demo.repository.RoleRepository;
 import zuka.cloud.demo.repository.UserRepository;
 
 @SpringBootApplication
@@ -19,23 +21,25 @@ public class MainApplication implements CommandLineRunner{
     UserRepository userRepository;
 
     @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
     PasswordEncoder passwordEncoder;
 
     @Override
     public void run(String... args) throws Exception {
-        User user1 = new User();
-        user1.setId(1);
-        user1.setUsername("admin");
-        user1.setPassword(passwordEncoder.encode("123"));
-        user1.setRole("ROLE_ADMIN");
-        userRepository.save(user1);
+        User user = new User();
+        user.setId(1);
+        user.setUsername("admin");
+        user.setPassword(passwordEncoder.encode("123"));
+        userRepository.save(user);
 
-        User user2 = new User();
-        user2.setId(2);
-        user2.setUsername("user");
-        user2.setPassword(passwordEncoder.encode("123"));
-        user2.setRole("ROLE_USER");
-        userRepository.save(user2);
+        Role role = new Role();
+        role.setId(1);
+        role.setName("ROLE_ADMIN");
+        role.setUsername(user.getUsername());
+        role.setUserId(user.getId());
+        roleRepository.save(role);
     }
 }
 
